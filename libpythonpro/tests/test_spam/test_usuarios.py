a87bfa7eb344100f8a4735ad8f9a2acebb26cfee
Flávio Borges <flyfmb@hotmail.com>
2021-05-31 17:51:27 -0300

    Emulado escopo da fixture scope, module ou session close #46

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,26 +1,8 @@
-import pytest
-
-from libpythonpro.spam.db import Conexao
 from libpythonpro.spam.modelos import Usuario
 
 
 # scope='session', executa a sessao apenas 1 vez
 # scope='module', executa a função apenas 1 vez
-@pytest.fixture(scope='session')
-def conexao():
-    # Setup
-    conexao_obj = Conexao()
-    yield conexao_obj  # função geradora
-    # Tear Down
-    conexao_obj.fechar()
-
-
-@pytest.fixture
-def sessao(conexao):
-    sessao_obj = conexao.gerar_sessao()
-    yield sessao_obj
-    sessao_obj.roll_back()
-    sessao_obj.fechar()
 
 
 def test_salvar_usuario(sessao):
